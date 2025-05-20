a682ab2845ce412bfe98ae5a013ec668d485c593
Dennis Kinder <dennis_kinder_ii@hotmail.com>
2025-05-19 21:17:22 -0600

    Initial Commit

---
+++ YouAreGreat/YouAreGreatApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreGreatApp.swift
+//  YouAreGreat
+//
+//  Created by Dennis Kinder on 5/19/25.
+//
+
+import SwiftUI
+
+@main
+struct YouAreGreatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
